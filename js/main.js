607d96e1e06cd50534b49fdaee9debdd7d9b43f1
SeifeldinAlaa <seifeldin.alaa@gmail.com>
2021-02-27 22:39:48 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Implementation of google map
+function initMap() {
+    // The location of Uluru
+    var cairo = { lat: 30.044420, lng: 31.235712 };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: cairo,
+    });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: cairo,
+        map: map
+    });
+
+}
+
+//Smoothscrolling
+$("#navbar a, .btn").on("click", function(event) {
+
+
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate({ scrollTop: $(hash).offset().top - 100 }, 800);
+    }
+});
+
+
+// Navbar Opacity
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+});
